64cf7c65b18c01dec177032beff45f31a427029c
Tuomo <tuokri@tuta.io>
2020-03-14 18:30:44 +0200

    Add support for multiple WebAdmin instances

--- examples/unranked_discord_alert/ranked_alert.py
+++ examples/unranked_discord_alert/ranked_alert.py
@@ -7,12 +7,26 @@ from rs2wapy import RS2WebAdmin
 # Set this to True to enable this example application.
 RUN = False
 
+
+def get_env_vars(pat: str) -> list:
+    i = 1
+    env_vars = []
+    while True:
+        try:
+            key = pat.replace("#", str(i))
+            env_vars.append(os.environ[key])
+            i += 1
+        except KeyError:
+            break
+    return env_vars
+
+
 # These variables are pulled from the runtime environment,
 # don't store passwords or secret information in this file!
-WEBADMIN_USERNAME = os.environ["WEBADMIN_USERNAME"]
-WEBADMIN_PASSWORD = os.environ["WEBADMIN_PASSWORD"]
-WEBADMIN_URL = os.environ["WEBADMIN_URL"]
-DISCORD_WEBHOOK_URL = os.environ["DISCORD_WEBHOOK_URL"]
+WEBADMIN_USERNAMES = get_env_vars("WEBADMIN_USERNAME_#")
+WEBADMIN_PASSWORDS = get_env_vars("WEBADMIN_PASSWORD_#")
+WEBADMIN_URLS = get_env_vars("WEBADMIN_URL_#")
+DISCORD_WEBHOOK_URLS = get_env_vars("DISCORD_WEBHOOK_URL_#")
 
 # Paste the role IDs here that you want to get pinged
 # when the unranked alert message is sent.
@@ -34,34 +48,39 @@ ALERT_INTERVAL = 60 * 15
 
 
 def main():
-    wa = RS2WebAdmin(
-        username=WEBADMIN_USERNAME,
-        password=WEBADMIN_PASSWORD,
-        webadmin_url=WEBADMIN_URL,
-    )
-    print("RS2WebAdmin initialized", flush=True)
+    web_admins = []
+
+    for wa_uname, wa_pw, wa_url in zip(
+            WEBADMIN_USERNAMES, WEBADMIN_PASSWORDS, WEBADMIN_URLS):
+        web_admins.append(RS2WebAdmin(
+            username=wa_uname,
+            password=wa_pw,
+            webadmin_url=wa_url,
+        ))
+
+    print(f"{len(web_admins)} RS2WebAdmin instances initialized", flush=True)
 
     # Setup roles we want to ping into a string.
     role_pings = " ".join(PING_DISCORD_ROLES)
 
     while True:
         try:
+            for wa, wh_url in zip(web_admins, DISCORD_WEBHOOK_URLS):
+                # Get server information from WebAdmin.
+                cg = wa.get_current_game()
+                server_name = cg.info["Server Name"]
 
-            # Get server information from WebAdmin.
-            cg = wa.get_current_game()
-            server_name = cg.info["Server Name"]
+                if not cg.ranked:
+                    print(f"'{server_name}' unranked, posting message to Discord", flush=True)
 
-            if not cg.ranked:
-                print("Server unranked, posting message to Discord", flush=True)
+                    # Format our warning message.
+                    message = f"Warning, server '{server_name}' is unranked! {role_pings}"
 
-                # Format our warning message.
-                message = f"Warning, server '{server_name}' is unranked! {role_pings}"
+                    # Post the warning message to Discord.
+                    webhook = DiscordWebhook(url=wh_url, content=message)
+                    webhook.execute()
 
-                # Post the warning message to Discord.
-                webhook = DiscordWebhook(url=DISCORD_WEBHOOK_URL, content=message)
-                webhook.execute()
-
-                time.sleep(ALERT_INTERVAL)
+                    time.sleep(ALERT_INTERVAL)
 
             # Sleep so that poll is performed every POLL_INTERVAL.
             time.sleep(POLL_INTERVAL - time.time() % POLL_INTERVAL)
